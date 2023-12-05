e2ce145631c458602747e6003fdc425dba528cc7
Andreas Monitzer <andreas@monitzer.com>
2023-12-05 01:14:59 +0100

    Solution for day4b.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -12,35 +12,53 @@ struct Args {
     filename: PathBuf,
 }
 
+#[derive(Default, Debug)]
+struct Scratchcard {
+    win_count: usize,
+    card_count: usize,
+}
+
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
     let file = File::open(args.filename)?;
     let reader = BufReader::new(file);
 
-    let sum = reader
+    let mut cards: Vec<_> = reader
         .lines()
         .map(|line| {
             let line = line.unwrap();
             let Some((_, numbers)) = line.split_once(": ") else {
-                return 0;
+                return Scratchcard::default();
             };
             let Some((winning, have)) = numbers.split_once(" | ") else {
-                return 0;
+                return Scratchcard::default();
             };
             let winning: HashSet<_> = winning
                 .split_whitespace()
                 .map(|n| n.parse::<usize>().unwrap())
                 .collect();
-            let winners_count = have
+            let win_count = have
                 .split_whitespace()
                 .filter(|&n| winning.contains(&n.parse::<usize>().unwrap()))
                 .count();
-            if winners_count == 0 {
-                0
-            } else {
-                2usize.pow((winners_count - 1) as u32)
+            Scratchcard {
+                win_count,
+                card_count: 1,
             }
+        })
+        .collect();
+
+    let sum = (0..cards.len())
+        .map(|idx| {
+            let Scratchcard {
+                win_count,
+                card_count,
+            } = cards[idx];
+            for won in cards[(idx + 1)..(idx + 1 + win_count)].iter_mut() {
+                won.card_count += card_count;
+            }
+            card_count
         })
         .sum::<usize>();
 
